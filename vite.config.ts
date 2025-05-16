68bd8715185ea747d5e0d3f1aff8da2abe5a6fd7
BibekBhusal0 <bibekbhusal04@gmail.com>
2025-05-16 12:42:08 +0545

    feat: Deploy to GitHub Pages 🚀

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import {defineConfig} from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import vitePluginInjectDataLocator from "./plugins/vite-plugin-inject-data-locator";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), vitePluginInjectDataLocator()],
-});
+  base: "/heroui-portfolio-template",
+})
